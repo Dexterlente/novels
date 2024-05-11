9ff13a2e6d4c4b3593ec6480365ccc3ee8921794
Dexter <dexter.lente18@gmail.com>
2024-05-11 15:54:37 +0800

    FIx

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from app.models import novels, chapters
 from app.pagination import paginate_query
-from app.views import serialize_novels
+from app.views import serialize_chapters, serialize_novels
 
 routes = Blueprint('views', __name__)
 
@@ -13,6 +13,22 @@ def get_novels():
 
     return jsonify({
         'novels': serialized_novels,
+        'total_pages': pagination.pages,
+        'current_page': pagination.page,
+        'total_items': pagination.total
+    })
+
+@routes.route('/get-chapters/<int:novel_id>', methods=['GET'])
+def get_chapters(novel_id):
+    page_number = request.args.get('page', 1, type=int)
+    pagination = paginate_query(chapters.query.filter_by(novel_id=novel_id), page_number)
+    serialized_chapters = serialize_chapters(pagination.items)
+
+            # params is page i dont know why the fuck it was not current_page
+            # sample http://localhost:5000/get-chapters/100?page=1
+
+    return jsonify({
+        'chapters': serialized_chapters,
         'total_pages': pagination.pages,
         'current_page': pagination.page,
         'total_items': pagination.total
--- app/views.py
+++ app/views.py
@@ -15,3 +15,38 @@ def serialize_novels(novels_list):
         }
         serialized_novels.append(serialized_novel)
     return serialized_novels
+
+
+
+def serialize_chapters(chapters_list, novel_id=None):
+    """
+    Serialize a list of chapters objects into a list of dictionary representations.
+    If novel_id is provided, only chapters belonging to that novel will be included.
+    """
+    serialized_chapters = []
+    chapter_numbers = {}
+    
+    for chapter in chapters_list:
+        if novel_id is not None and chapter.novel_id != novel_id:
+            continue
+
+        if chapter.novel_id not in chapter_numbers:
+            # Initialize chapter_number for a novel_id if it's not present
+            chapter_numbers[chapter.novel_id] = 1
+
+        # Assign chapter_number from the dictionary
+        chapter_number = chapter_numbers[chapter.novel_id]
+
+        # Update chapter_number for the next chapter
+        chapter_numbers[chapter.novel_id] += 1
+
+        serialized_chapter = {
+            'chapter_id': chapter.chapter_id,
+            'novel_id': chapter.novel_id,
+            'timestamp': chapter.timestamp,
+            'chapter_number': chapter_number
+        }
+        serialized_chapters.append(serialized_chapter)
+
+    return serialized_chapters
+
